07bcb40dcc183f137ed05cc3f5d34705ea4de157
Vadim Ignatov <ignatov0131@gmail.com>
2024-01-31 22:30:35 +0300

    feat(App): getting currencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
+import ConverterWidget from "./components/converter-widget/ConverterWidget.tsx";
+
+import styles from "./App.module.css"
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {setCurrencies} from "@/services/currency.ts";
+import {RootState} from "@/store.ts";
+
 function App() {
-  return (
-    <div>
-        <div>
-            <h1>Currency Converter</h1>
+	const currencies = useSelector((state: RootState) => state.currency.currencyList)
+	const dispatch = useDispatch();
+	const getCurrentCurrencies = async () => {
+		try {
+			const data = await fetch("https://currency-exchange.p.rapidapi.com/listquotes", {
+				headers: {
+					"X-RapidAPI-Key": import.meta.env.VITE_API_KEY_CURRENCIES,
+					"X-RapidAPI-Host": "currency-exchange.p.rapidapi.com"
+				}
+			}).then(res => res.json())
 
-            <p>
-                Check live rates, set rate alerts, receive notifications and more.
-            </p>
-        </div>
+			dispatch(setCurrencies(data))
+		} catch (e) {
+			console.warn(e);
+		}
+	};
 
-        <div>
+	useEffect(() => {
+		getCurrentCurrencies();
+	}, [])
 
-        </div>
+	return (
+		<div className={styles.App}>
+			<div className={styles.top}>
+				<h1 className={styles.topTitle}>Currency Converter</h1>
 
-        <div>
-            <p>
-                Indicative Exchange Rate
-            </p>
+				<p className={styles.topSubtitle}>
+					Check live rates, set rate alerts, receive notifications and more.
+				</p>
+			</div>
 
-            <strong>1 SGD = 0.7367 USD</strong>
-        </div>
-    </div>
-  )
+			{ currencies.length > 0 && <ConverterWidget /> }
+		</div>
+	)
 }
 
 export default App
