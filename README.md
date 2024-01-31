8c80343d81b97dea68d10bf54edbb4d1184aa27c
Vadim Ignatov <ignatov0131@gmail.com>
2024-01-31 17:35:18 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Onez.io testcase app
+
+## Requirements
+``Node.js v20.10``
+
+## Installation
+- Clone this repository
+- Run ``npm i``
+- Run ``npm run dev``
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div>
+        <div>
+            <h1>Currency Converter</h1>
+
+            <p>
+                Check live rates, set rate alerts, receive notifications and more.
+            </p>
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+            <p>
+                Indicative Exchange Rate
+            </p>
+
+            <strong>1 SGD = 0.7367 USD</strong>
+        </div>
+    </div>
+  )
+}
+
+export default App
